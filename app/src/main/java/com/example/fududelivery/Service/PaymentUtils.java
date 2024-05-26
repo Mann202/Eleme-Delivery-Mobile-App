84534a4fab2baeb64ff236240350c7ebfce4cad9
Trương GIa Mẫn <21521115@gm.uit.edu.vn>
2024-05-26 14:30:56 +0700

    Setup for GG Pay Payment Transaction

---
+++ app/src/main/java/com/example/fududelivery/Service/PaymentUtils.java
@@ -0,0 +1,90 @@
+package com.example.fududelivery.Service;
+
+import android.content.Context;
+
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PaymentUtils {
+    private static JSONObject getBaseRequest() throws JSONException {
+        return new JSONObject()
+                .put("apiVersion", 2)
+                .put("apiVersionMinor", 0);
+    }
+
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        return new JSONObject()
+                .put("type", "PAYMENT_GATEWAY")
+                .put("parameters", new JSONObject()
+                        .put("gateway", "example")
+                        .put("gatewayMerchantId", "exampleGatewayMerchantId")
+                );
+    }
+
+    private static JSONArray getAllowedCardNetworks() {
+        return new JSONArray()
+                .put("AMEX")
+                .put("DISCOVER")
+                .put("INTERAC")
+                .put("JCB")
+                .put("MASTERCARD")
+                .put("VISA");
+    }
+
+    private static JSONObject getBaseCardPaymentMethod() throws JSONException {
+        return new JSONObject()
+                .put("type", "CARD")
+                .put("parameters", new JSONObject()
+                        .put("allowedAuthMethods", getAllowedCardAuthMethods())
+                        .put("allowedCardNetworks", getAllowedCardNetworks())
+                        .put("billingAddressRequired", true)
+                        .put("billingAddressParameters", new JSONObject()
+                                .put("format", "FULL")
+                        )
+                );
+    }
+    private static JSONArray getAllowedCardAuthMethods() {
+        return new JSONArray()
+                .put("PAN_ONLY")
+                .put("CRYPTOGRAM_3DS");
+    }
+
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        return getBaseCardPaymentMethod()
+                .put("tokenizationSpecification", getGatewayTokenizationSpecification());
+    }
+
+    public static PaymentsClient createPaymentsClient(Context context) {
+        Wallet.WalletOptions walletOptions =
+                new Wallet.WalletOptions.Builder().setEnvironment(Wallet.TEST).build();
+        return Wallet.getPaymentsClient(context, walletOptions);
+    }
+
+    public static JSONObject getIsReadyToPayRequest() {
+        try {
+            return getBaseRequest()
+                    .put("allowedPaymentMethods", new JSONArray().put(getBaseCardPaymentMethod()));
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+
+    private static JSONObject getTransactionInfo(String price) throws JSONException {
+        return new JSONObject()
+                .put("totalPrice", price)
+                .put("totalPriceStatus", "FINAL")
+                .put("countryCode", Constants.COUNTRY_CODE)
+                .put("currencyCode", Constants.CURRENCY_CODE)
+                .put("checkoutOption", "COMPLETE_IMMEDIATE_PURCHASE");
+    }
+
+    private static JSONObject getMerchantInfo() throws JSONException {
+        return new JSONObject().put("merchantName", "Example Merchant");
+    }
+
+
+}
