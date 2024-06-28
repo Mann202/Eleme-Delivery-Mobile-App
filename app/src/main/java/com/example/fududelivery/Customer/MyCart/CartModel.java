c91deaed00ea8c8a9a91661cda1cc5de75dd5bfb
nguyenlz <146945072+nguyenlz@users.noreply.github.com>
2024-06-28 12:14:24 +0700

    Load Cart, Total price, quantity

--- app/src/main/java/com/example/fududelivery/Customer/MyCart/CartModel.java
+++ app/src/main/java/com/example/fududelivery/Customer/MyCart/CartModel.java
@@ -1,35 +1,31 @@
 package com.example.fududelivery.Customer.MyCart;
 
 public class CartModel {
+    String UserID;
     String CartID;
-    String FoodID;
-    String FoodName;
-    String FoodCate;
-    String Quantity;
-    Float TotalPrice;
 
     public CartModel() {
     }
 
-    public CartModel(String cartID, String foodID, String foodName, String foodCate, String quantity, Float totalPrice) {
+    public CartModel(String userID, String cartID) {
+        UserID = userID;
         CartID = cartID;
-        FoodID = foodID;
-        FoodName = foodName;
-        FoodCate = foodCate;
-        Quantity = quantity;
-        TotalPrice = totalPrice;
     }
 
     @Override
     public String toString() {
         return "CartModel{" +
-                "CartID='" + CartID + '\'' +
-                ", FoodID='" + FoodID + '\'' +
-                ", FoodName='" + FoodName + '\'' +
-                ", FoodCate='" + FoodCate + '\'' +
-                ", Quantity='" + Quantity + '\'' +
-                ", TotalPrice='" + TotalPrice + '\'' +
+                "UserID='" + UserID + '\'' +
+                ", CartID='" + CartID + '\'' +
                 '}';
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
     }
 
     public String getCartID() {
@@ -38,45 +34,5 @@ public class CartModel {
 
     public void setCartID(String cartID) {
         CartID = cartID;
-    }
-
-    public String getFoodID() {
-        return FoodID;
-    }
-
-    public void setFoodID(String foodID) {
-        FoodID = foodID;
-    }
-
-    public String getFoodName() {
-        return FoodName;
-    }
-
-    public void setFoodName(String foodName) {
-        FoodName = foodName;
-    }
-
-    public String getFoodCate() {
-        return FoodCate;
-    }
-
-    public void setFoodCate(String foodCate) {
-        FoodCate = foodCate;
-    }
-
-    public String getQuantity() {
-        return Quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        Quantity = quantity;
-    }
-
-    public Float getTotalPrice() {
-        return TotalPrice;
-    }
-
-    public void setTotalPrice(Float totalPrice) {
-        TotalPrice = totalPrice;
     }
 }
